e89e22de59ff9c32db711be9e14cfccbf632df83
EL Tegani <etegani@localhost.localdomain>
2019-09-08 10:18:04 +0200

    initial commit

---
+++ app/src/main/java/com/expensemoitor/c_certificates/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.expensemoitor.c_certificates
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
